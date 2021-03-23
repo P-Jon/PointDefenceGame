9f02a08ca3a70594795c60a36e2c369a9cbbb548
Jonathon Cross <j.cross3@lancaster.ac.uk>
2021-03-23 18:50:20 +0000

    Initial Commit

---
+++ RaylibDemo/core/GameDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Raylib_cs;
+
+namespace RaylibDemo.core
+{
+    public class GameDemo
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            const int screenWidth = 1280;
+            const int screenHeight = 720;
+
+            Raylib.InitWindow(screenWidth, screenHeight, "DemoGame");
+        }
+    }
+}
