918c56df359e5937e411df8ba181f280e9d13b4c
Jonathon Cross <j.cross3@lancaster.ac.uk>
2021-04-08 23:20:17 +0100

    Adding AudioManager

---
+++ RaylibDemo/Audio/AudioManager.cs
@@ -0,0 +1,48 @@
+﻿using PointDefence.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Raylib_cs;
+using System.IO;
+
+namespace PointDefence.Audio
+{
+    public class AudioManager
+    {
+        private string _audioDirectory = GameData.localDir + "Audio/";
+        private Dictionary<string, Sound> _audioDictionary = new Dictionary<string, Sound>();
+
+        public AudioManager()
+        {
+            OpenAudioDevice();
+        }
+
+        private void LoadSoundsFromDirectory()
+        {
+            var files = Directory.GetFiles(_audioDirectory);
+
+            foreach (var file in files)
+            {
+                var fileWithoutExtension = Path.GetFileNameWithoutExtension(file);
+                var sound = Raylib.LoadSound(file);
+                _audioDictionary.Add(fileWithoutExtension, sound);
+            }
+        }
+
+        public void PlaySound(string sound)
+        {
+            if (_audioDictionary.ContainsKey(sound))
+                Raylib.PlaySound(_audioDictionary[sound]);
+        }
+
+        private void OpenAudioDevice()
+        {
+            Raylib.InitAudioDevice();
+        }
+
+        public void CloseAudioDevice()
+        {
+            Raylib.CloseAudioDevice();
+        }
+    }
+}
